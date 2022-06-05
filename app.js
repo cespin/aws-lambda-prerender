53052c93adf762b7d41515dbe755a7d185c05aa6
Carlos Espin <charly8182@outlook.com>
2022-06-05 00:04:08 -0400

    Deleted executablePath argument from puppeteer.launch()

--- app.js
+++ app.js
@@ -41,7 +41,6 @@ const wait = () => new Promise(resolve => {
 
 const prerender = async uri =>
     puppeteer.launch({
-        executablePath: 'google-chrome-stable',
         args: [
             '--allow-running-insecure-content',
             '--autoplay-policy=user-gesture-required',
